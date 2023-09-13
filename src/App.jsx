9d1e904b13b6cf16223bafe38f66fc9d2108e35c
PC-19 <>
2023-09-12 21:06:46 -0300

    Aula2

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState(0);
+  const [fruit, setFruit] = useState(0);
 
-  const myString = "Minha variável"
-  console.log(myString)
+  const handleAddTwo = () => {
+    setValue(value + 2);
+  }
+
+  const handleClickFruit = (currentFruit) => {
+    setFruit(currentFruit);
+  }
 
   return (
     <div>
-      <h1>Hello Word</h1>
-      <p>Welcome</p>
+      <button onClick={handleAddTwo}>Double Counter</button>
+      <h1>Current Value: {value}</h1>
+    <hr />
+    <button onClick={() => handleClickFruit("Maçã")}>Maçã</button>
+    <button onClick={() => handleClickFruit("Banana")}>Banana</button>
+    <button onClick={() => handleClickFruit("Morango")}>Morango</button>
+    {fruit && (<h1>Clicked on: {fruit}</h1>)}
+    {!fruit && (<h1>Click on some of the fruits</h1>)}
     </div>
   )
 }
-
-  const myArray = ["Fruta 1",
-                   "Fruta 2",
-                   "Fruta 3",
-                   "Caja",
-                   "Maçã"];
-
-  myArray.map((fruits) => {
-    return console.log(fruits)
-  });
-
-  const myArrayObjects = [
-    {id: 1,
-     name: "José",
-     age: 15
-    },
-    {id: 2,
-      name: "Maria",
-      age: 43
-     },
-     {id: 3,
-      name: "Joao",
-      age: 19
-     },
-  ];
-
-myArrayObjects.map((person) => {
-  return console.log(`${person.name} tem ${person.age}`)
-})
-
-const quantity = 4;
-
-const fruitsResultFind = myArray.find(
-  (fruits) => fruits.length === quantity);
-console.log("Array of Length:", quantity);
-console.log("Single Result (.find):", fruitsResultFind);
-
-const fruitsResultFilter = myArray.filter(
-  (fruits) => fruits.length === quantity);
-console.log("Multiple Result (.filter):", fruitsResultFilter);
-// Observe that the filter method returns an array
-
-const search = ["ç","aj"]
-const result = myArray.filter(
-  (currentFruit) => currentFruit.includes(search)
-);
-console.log(result)
 
 export default App
