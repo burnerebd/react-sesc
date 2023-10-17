57c722f2ed2c94ec7c0c1c573f9d241a0637b287
burnerebd <husk321@proton.me>
2023-10-17 14:23:19 -0300

    Aula 6: Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+<<<<<<< HEAD
 import Lista from './components/Lista.jsx'
 // import TestUseState from './Exercises/Class2.jsx'
+=======
+>>>>>>> b4b66e2 (Aula 6: Update)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
