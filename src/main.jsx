5b343014f1ebfbee6ba87177459bf40a8c5d091f
PC-19 <>
2023-10-24 20:57:24 -0300

    Aula 8

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
 import App from './App.jsx'
+import {HomePage, TasksPage, ProjectsPage} from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+
+  {
+    path: "/tasks",
+    element: <TasksPage/>
+  },
+
+  {
+    path: "/projects",
+    element: <ProjectsPage/>
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
