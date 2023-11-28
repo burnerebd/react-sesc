ab11e5923f6356a7655206b57e155abe3706f5b3
burnerebd <husk321@proton.me>
2023-11-28 10:57:46 -0300

    Non-breaking changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import App from "./App.jsx";
 import { HomePage, TasksPage, ProjectsPage } from "./pages";
+import './App.css';
 
 const router = createBrowserRouter([
   {
